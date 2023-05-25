267e2b61e9e6a928e91b94f34a8ff59db322124f
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-05-25 13:51:27 +0300

    create navbar component and register it into app

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="container-fluid">
+    <NavbarComponent />
+    <HomeComponent />
   </div>
 </template>
 
 <script>
-
+import HomeComponent from './components/props/lab2/HomeComponent.vue';
+import NavbarComponent from './components/props/lab2/NavbarComponent.vue';
 
 export default {
   name: 'App',
+  components:{
+    HomeComponent,
+    NavbarComponent,
+  }
 };
 </script>
 
--- src/components/props/lab2/HomeComponent.vue
+++ src/components/props/lab2/HomeComponent.vue
@@ -1,6 +1,6 @@
 <template >
-    <div>
-        
+    <div class="container-fluid mt-5 pt-3">
+        hi from home
     </div>
 </template>
 <script>
