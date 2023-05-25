6ac62533c719230a42672efa7fe17a3526873594
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-05-25 17:38:02 +0300

    create moviedetails page

--- src/components/props/lab2/MovieDetailsComponent.vue
+++ src/components/props/lab2/MovieDetailsComponent.vue
@@ -1,13 +1,50 @@
 <template>
-    <div class="container-fluid">
-        this is movie details
+    <div class="container-fluid mt-5 py-4">
+      <div class="row">
+        <div class="col-md-6 mx-auto">
+          <h2 class="mb-4">{{ movie.Title }}</h2>
+          <div class="card">
+            <div class="card-body">
+              <p><strong>Release Date:</strong> {{ movie["Release Date"] }}</p>
+              <p><strong>Rating:</strong> {{ movie.Rating }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</template>
-<script>
-export default {
-    
-}
-</script>
-<style>
-    
-</style>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  
+  export default {
+    data() {
+      return {
+        movie: {
+          Title: "",
+          "Release Date": "",
+          Rating: "",
+        },
+      };
+    },
+    methods: {
+      getMovieById() {
+        const id = this.$route.params.id;
+        axios
+          .get(`http://localhost:3000/movies/${id}`)
+          .then((response) => {
+            this.movie = response.data;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      },
+    },
+    created() {
+      this.getMovieById();
+    },
+  };
+  </script>
+  
+  <style>
+  </style>
