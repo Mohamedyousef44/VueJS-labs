c76f87b9e09cbaea1a214e8f2d9029c0e8958c23
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-05-25 14:32:16 +0300

    create routes for home , about and profile components

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="container-fluid">
     <NavbarComponent />
-    <HomeComponent />
+    <router-view></router-view>
     <FooterComponent />
   </div>
 </template>
 
 <script>
-import HomeComponent from './components/props/lab2/HomeComponent.vue';
 import NavbarComponent from './components/props/lab2/NavbarComponent.vue';
 import FooterComponent from './components/props/lab2/FooterComponent.vue'
 
 export default {
   name: 'App',
   components:{
-    HomeComponent,
     NavbarComponent,
     FooterComponent,
   }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
-import { VueRouter } from 'vue-router'
+import { createWebHistory,createRouter } from 'vue-router';
 import HomeComponent from './components/props/lab2/HomeComponent.vue'
+import AboutComponent from './components/props/lab2/AboutComponent.vue'
 import App from './App.vue'
 
-const router = new VueRouter({
-    routes: [
-      { path: '/', component: HomeComponent },
-    //   { path: '/about', component: About }
-    ]
-  });
-
+const routes = [
+    { 
+        path: '/', component: HomeComponent
+    },
+    { 
+        path: '/about', component: AboutComponent
+    },
+    // {
+    //     path:'/:NotFound(.*)*',component:errorApp,meta:{hideNavbar:true}
+    // }
+  ]
+const router = createRouter({
+    history:createWebHistory(),
+    routes
+})
 createApp(App).use(router).mount('#app')
