cf000c51064a9873692cd205ed2d78342f355f39
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-05-25 14:25:26 +0300

    create about component

---
+++ src/components/props/lab2/AboutComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class='container-fluid mt-4 pt-4'>
+        hi from about page
+    </div>
+</template> 
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
