c9537a834d2e945882b40315a097c535b9dd38db
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-05-25 14:15:17 +0300

    add background-image in home and add some style to it

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div class="container-fluid">
     <NavbarComponent />
     <HomeComponent />
+    <FooterComponent />
   </div>
 </template>
 
 <script>
 import HomeComponent from './components/props/lab2/HomeComponent.vue';
 import NavbarComponent from './components/props/lab2/NavbarComponent.vue';
+import FooterComponent from './components/props/lab2/FooterComponent.vue'
 
 export default {
   name: 'App',
   components:{
     HomeComponent,
     NavbarComponent,
+    FooterComponent,
   }
 };
 </script>
--- src/components/props/lab2/HomeComponent.vue
+++ src/components/props/lab2/HomeComponent.vue
@@ -1,6 +1,9 @@
 <template >
-    <div class="container-fluid mt-5 pt-3">
-        hi from home
+    <div class="container-fluid mt-5 pt-3 cover-banner d-flex justify-content-center align-items-center">
+        <div>
+            <h1 class="text-light">Start Your Journey</h1>
+            <button class="btn btn-light">Get Started</button>
+        </div>
     </div>
 </template>
 <script>
@@ -9,5 +12,11 @@ export default {
 }
 </script>
 <style>
+.cover-banner{
+    background-image: url('@/assets/banner (2).jpg');
+    min-height: 100vh;
+    width: 100%;
+    object-fit: cover;
+}
     
 </style>
