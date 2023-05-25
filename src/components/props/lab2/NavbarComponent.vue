685b1865ba7bb1d7c4c62cf6a3b38d1cad68297a
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-05-25 14:23:52 +0300

    install vue-router package and setup the routes env

--- src/components/props/lab2/NavbarComponent.vue
+++ src/components/props/lab2/NavbarComponent.vue
@@ -9,10 +9,13 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <router-link class="nav-link active" aria-current="page" to="/" >Home</router-link>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                        <router-link class="nav-link active" aria-current="page" to="/about" >About</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link active" aria-current="page" to="/profile" >Profile</router-link>
                     </li>
                 </ul>
                 <form class="d-flex" role="search">
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
+import { VueRouter } from 'vue-router'
+import HomeComponent from './components/props/lab2/HomeComponent.vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const router = new VueRouter({
+    routes: [
+      { path: '/', component: HomeComponent },
+    //   { path: '/about', component: About }
+    ]
+  });
+
+createApp(App).use(router).mount('#app')
