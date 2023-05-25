c803ce55b5be02528a4ebf5aed7339bcf22f3cab
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-05-25 13:40:58 +0300

    create home template

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="container-fluid">
-    <FormComponent />
   </div>
 </template>
 
 <script>
-import FormComponent from './components/props/FormComponent.vue';
+
 
 export default {
   name: 'App',
-  components: {
-    FormComponent
-  },
 };
 </script>
 
